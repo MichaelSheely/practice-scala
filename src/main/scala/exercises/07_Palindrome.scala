a83841d1b5fde1d7457fd62d258948b5b0801ea9
Michael Sheely <msheely@hmc.edu>
2016-09-26 01:52:56 -0700

    Finished palindrome

--- src/main/scala/exercises/07_Palindrome.scala
+++ src/main/scala/exercises/07_Palindrome.scala
@@ -1,6 +1,16 @@
 package exercises
 
 object Palindrome {
+  /**
+   * Returns a set of the alphabetic characters
+   */
+  val alphaChars : Set[Char] = (('a' to 'z') ++ ('A' to 'Z')).toSet
+  /**
+   * Returns true iff char is alphabetic
+   */
+  def isAlpha(char: Char): Boolean = {
+    alphaChars contains char
+  }
   /** 
    *  True if the string is a palindrome, after removing all non-alphabetic 
    *  characters (e.g., spaces, numbers, and punctuation).
@@ -8,6 +18,10 @@ object Palindrome {
    *  Hint: the Scala collections API is your friend
    *  http://docs.scala-lang.org/overviews/collections/overview.html 
    */
-  def isPalindrome(s: String): Boolean = throw new NotImplementedError
+  def isPalindrome(s: String): Boolean = {
+    val alpha_chars = s.toLowerCase.toList.filter(isAlpha)
+    val reversal = alpha_chars.reverse
+    alpha_chars == reversal
+  }
 
 }
